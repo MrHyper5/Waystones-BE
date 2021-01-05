44fb0c3ec96cc69c2bcf6eb8a0912b5be41c0f74
MrHyper5 <74113266+MrHyper5@users.noreply.github.com>
2021-01-05 15:34:30 +0200

    update block.js

--- dev/block.js
+++ dev/block.js
@@ -1,17 +1,21 @@
-
+//регистрация специального типа блока
 var Obelisk = Block.createSpecialType({
      lightlevel: 3,
      lightopacity: 15,
      explosionres: 9,
      destroytime: 2
 });
-
+//регистрация айди блока
 IDRegistry.genBlockID("obelisk");
-
-Block.createBlock("obelisk", [
-	{name: "obelisk", texture: [["kk", 4]], inCreative: true}
-], Obelisk);
-
+//создание блока
+Block.createBlock("obelisk",
+[
+	{name: "obelisk", texture: [[
+	  "plants", 0
+]], inCreative: true}
+],
+Obelisk);
+//присвоение материала блоку
 ToolAPI.registerBlockMateri(
-  BlockID.myBlock, "stone", 1
+  BlockID.obelisk, "stone", 2
 );
