1a74875bc67f3d46027a39c74d9a83111f498f1b
MrHyper5 <74113266+MrHyper5@users.noreply.github.com>
2021-01-05 15:22:54 +0200

    update block.js

--- dev/block.js
+++ dev/block.js
@@ -1,3 +1,10 @@
 
+var sandcrystal = Block.createSpecialType({
+     lightlevel: 5,
+     lightopacity: 15,
+     explosionres: 9,
+     destroytime: 0.5
+});
+
 IDRegistry.genBlockID("obelisk");
 block.createblock
