3c0820282eacea762f6dd40aa284934390f891a2
MrHyper5 <74113266+MrHyper5@users.noreply.github.com>
2021-01-05 15:25:16 +0200

    update block.js

--- dev/block.js
+++ dev/block.js
@@ -1,10 +1,13 @@
 
-var sandcrystal = Block.createSpecialType({
-     lightlevel: 5,
+var Obelisk = Block.createSpecialType({
+     lightlevel: 3,
      lightopacity: 15,
      explosionres: 9,
-     destroytime: 0.5
+     destroytime: 2
 });
 
 IDRegistry.genBlockID("obelisk");
-block.createblock
+
+Block.createBlock("obelisk", [
+	{name: "obelisk", texture: [["kk", 4]], inCreative: true}
+], Obelisk);
