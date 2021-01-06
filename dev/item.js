1f0edc7b18ee149c5b91a69688afeeb01c5d58ad
MrHyper5 <74113266+MrHyper5@users.noreply.github.com>
2021-01-06 19:56:09 +0200

    update item.js

--- dev/item.js
+++ dev/item.js
@@ -1,1 +1,26 @@
 
+//регистрация айди предмета
+IDRegistry.genItemID("Waystone");
+IDRegistry.genItemID("warpStone");
+//создаём предмет
+Item.createItem("Waystone", "Waystone", {
+  name: "waystone", meta: 0
+},
+{stack:64});
+Item.createItem("warpStone", "warpStone", {
+  name: "warpstone", meta: 0
+},
+{stack:1});
+//рецепты
+Callback.addCallback('LevelPreLoaded', function () {
+  Recipes.addShaped({id: ItemID.Waystone, count: 1, data: 0}, [
+	" b ",
+	"bcb",
+	"aaa"
+], ['a', 98, 0, 'b', 49, 0, 'c', 264, 0]);
+  Recipes.addShaped({id: ItemID.warpStone, count: 1, data: 0}, [
+	"bab",
+	"aca",
+	"bab"
+], ['a', 368, 0, 'b', 351, 13, 'c', 388, 0]);
+});
